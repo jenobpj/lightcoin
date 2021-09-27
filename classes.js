7a3150dfa3c7412749a6bc3a9e8acd86c7c3001a
jenobpj <jenobgit@gmail.com>
2021-09-27 00:07:10 -0400

    added

---
+++ classes.js
@@ -0,0 +1,34 @@
+class Account{
+  constructor(username){
+    this.username=username;
+    this.balance=0;
+  }
+}
+class Transaction{
+  constructor(amount,account){
+    this.amount = amount;
+    this.account = account; 
+  }
+  commit(){
+    this.account.balance+=this.value;
+  }
+}
+class Deposit extends Transaction {
+  get value(){
+    return this.amount;
+  }
+}
+class Withdrawal extends Transaction{
+  get value(){
+    return -this.amount;
+  }
+}
+const myAccount=new Account('jenob');
+console.log('Starting Balance:',myAccount.balance)
+
+const t1=new Deposit(120.00,myAccount);
+t1.commit();
+const t2=new Withdrawal(50.00,myAccount);
+t2.commit();
+console.log('ending balance',myAccount.balance)
+
